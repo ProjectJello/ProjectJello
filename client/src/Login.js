5bbcf81bb4feb461c1691c105c095d201a606f43
Clay Visscher <clayvisscher@gmail.com>
2017-11-09 16:46:25 -0500

    Final Login Design

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,21 +5,21 @@ import './Login.css';
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
+      <form className="Login">
           <div className = "Spacer">
             
             <img className ="Login-logo" alt="" src={fulllogo} />
           </div>
-          <div className = "Spacer"> 
+          <div className = "El-Container Spacer"> 
               <input className = "input"
               placeholder = 'Username' />
           </div>
-          <div className = "Spacer">
-            <button className ="button">
+          <div className = "El-Container Spacer">
+            <button className ="button" type = "submit">
               Login
               </button>
             </div>
-      </div>
+      </form>
     );
   }
 }
