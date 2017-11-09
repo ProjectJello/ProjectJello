3ca7f8a31228d22484383aefa349ae2e1a43a120
Joseph Chamberlain <josephlchamberlain@gmail.com>
2017-11-09 17:30:06 -0500

    Added in Project list functionality. Colorized links.

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import logo from './assets/JelloLogo.svg';
 import ProjectPlus from './assets/WhitePlus.svg';
 import './Sidebar.css';
-
+import Project from './Project.js';
 class Sidebar extends Component {
+
+  constructor() {
+    super()
+    this.ProjectList = [{ Name: "Proj1" },{ Name: "Proj2" },{ Name: "Proj3" }]
+  }
+
   render() {
     return (
       <div className="Sidebar">
         <img src={logo} className="App-Logo" alt="Jello" />
         <div className="Project">
             <h1 className="Project-Title">Projects</h1>
-            <a href className="Add-Project-Icon"> <img src={ProjectPlus} alt="Plus" id="WhitePlus" /> </a>
+            <a href="#" className="Add-Project-Icon"> <img src={ProjectPlus} alt="Plus" id="WhitePlus" /> </a>
         </div>
+
+        <table className="ProjectListTable">
+          {this.ProjectList.map((item, index) => (
+              <Project Project={item} FeedData={this.ProjectClicked} />
+            ))}
+        </table>
+
       </div>
     );
   }
+
+  ProjectClicked(project) {
+    alert("alert clicked" + project.Name);
+  }
+
 }
 
 export default Sidebar;
