da691f00b89c3bf9c91a833d7a85e8baadb6eabf
Joseph Chamberlain <josephlchamberlain@gmail.com>
2017-11-14 16:37:11 -0500

    Committing final changes before merge.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sidebar from './Sidebar.js';
+import Dashboard from './Dashboard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Sidebar">
-          <Sidebar />
-        </div>
+      	<Dashboard />
       </div>
     );
   }
