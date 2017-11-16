8034c7789bb6c1f38a4787f6d7a3424a437c51c4
Daniel Pratt <daniel@egosventures.com>
2017-11-16 16:21:17 -0500

    Created RiskStatus

---
+++ client/src/RiskStatus.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './RiskStatus.css';
+
+class RiskStatus extends Component {
+  render() {
+    return (
+      <div className="Severity"{this.props.Severity}>
+
+      </div>
+    );
+  }
+}
+
+export default RiskStatus;
