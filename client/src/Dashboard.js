ef7962c56cd498e00b39b92b26de004dd884ceb9
Daniel Pratt <daniel@egosventures.com>
2017-11-26 17:36:21 -0500

    Finished up UI for Risks. Next, need to add the ability to modify a risk

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -25,16 +25,16 @@ class Dashboard extends Component {
     this.ProjectData = [
     { 
       name: "Proj1",
-      owner: {name: "Joe",  pfp: "..public/examplePfp.png"},
-      members: [{name: "Joe",  pfp: "..public/examplePfp.png"},{name: "Joe",  pfp: "..public/examplePfp.png"}],
-      tasks: [{ name: "Task1", description: "Desc1", assignee: {name: "Daniel",  pfp: "..public/examplePfp.png"}, hours: 5.0, status: 0},{ name: "Task2", description: "Desc2", assignee: {name: "Joe",  pfp: "..public/examplePfp.png"}, hours: 2.0, status: 1}],
+      owner: {name: "Joe",  pfp: "../static/media/examplePfp.png"},
+      members: [{name: "Joe",  pfp: "../static/media/examplePfp.png"},{name: "Joe",  pfp: "../static/media/examplePfp.png"}],
+      tasks: [{ name: "Task1", description: "Desc1", assignee: {name: "Daniel",  pfp: "../static/media/examplePfp.png"}, hours: 5.0, status: 0},{ name: "Task2", description: "Desc2", assignee: {name: "Joe",  pfp: "../static/media/examplePfp.png"}, hours: 2.0, status: 1}],
       risks: [{ name: "Risk1", description: "Desc1", severity: 0}, { name: "Risk1", description: "Desc1", severity: 1}]
     },
     { 
       name: "Proj2",
-      owner: {name: "Sam",  pfp: "..public/examplePfp.png"},
-      members: [{name: "Joe",  pfp: "..public/examplePfp.png"},{name: "Joe",  pfp: "..public/examplePfp.png"}],
-      tasks: [{ name: "BLAH", description: "Desc1", assignee: {name: "Daniel",  pfp: "..public/examplePfp.png"}, hours: 5.0, status: 1},{ name: "Task2", description: "Desc2", assignee: {name: "Joe",  pfp: "..public/examplePfp.png"}, hours: 2.0, status: 2}],
+      owner: {name: "Sam",  pfp: "../static/media/examplePfp.png"},
+      members: [{name: "Joe",  pfp: "../static/media/examplePfp.png"},{name: "Joe",  pfp: "../static/media/examplePfp.png"}],
+      tasks: [{ name: "BLAH", description: "Desc1", assignee: {name: "Daniel",  pfp: "../static/media/examplePfp.png"}, hours: 5.0, status: 1},{ name: "Task2", description: "Desc2", assignee: {name: "Joe",  pfp: "../static/media/examplePfp.png"}, hours: 2.0, status: 2}],
       risks: [{ name: "BLAH", description: "Desc1", severity: 0}, { name: "Risk1", description: "Desc1", severity: 1}]
     }
     ]
