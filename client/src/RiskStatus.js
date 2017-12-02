8fa3ccc05f184d27063db0499ff685a3bb4329b9
Daniel Pratt <daniel@egosventures.com>
2017-12-02 15:30:36 -0500

    Added label to RiskStatus

--- client/src/RiskStatus.js
+++ client/src/RiskStatus.js
@@ -5,6 +5,9 @@ class RiskStatus extends Component {
   render() {
     return (
       <div className={"RiskSeverity" + this.props.Severity}>
+        <div className="RiskLabel">
+          <span>Status:</span>
+        </div>
         <div className="select-box" id="RiskText">
           <select onChange={this.statusOnChange.bind(this)}>
             <option selected={this.props.Severity === 0 && 'selected'} value="0">Addressed</option>
