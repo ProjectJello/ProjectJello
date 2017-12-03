ab8e10bc5dca77c0f808368334eb4261bca18524
Joseph Chamberlain <josephlchamberlain@gmail.com>
2017-12-03 14:40:31 -0500

    Added members list and owner to project view. Made fixture changes to user files.

--- client/src/ProjectInfoView.js
+++ client/src/ProjectInfoView.js
@@ -9,14 +9,37 @@ class ProjectInfoView extends Component {
   render() {
     return (
       <div className="ProjectInfoView">
+        <div className="ProjectHeaderAndMembers">
+
           <div className="ProjectInfoHeader">
-                <span className="ProjectTitle"><b>{this.props.ProjectInfoData.name}</b></span>  
+            <span className="ProjectTitle"><b>{this.props.ProjectInfoData.name}</b></span>  
           </div>
 
-          <div className="ProjectInfoDescription editable">
-            <p className="CreateDescriptionMargin">  Stuff and things </p>
-          </div>           
+          <div className="Members">
+            <div className="Owner">
+              <span>
+                Owner:
+                
+                  <img className="PFP" src={this.props.ProjectInfoData.owner.pfp} height="30px" width="30px" />
+              </span>
+            </div>
 
+            <div className="MembersList">
+              <span>
+                Members:
+                  {this.props.ProjectInfoData.members.map((item, index) => (
+                  <img className="PFP" src={'..' + item.pfp} height="30px" width="30px" />
+                ))}
+              </span>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className="ProjectInfoDescription editable">
+          <p className="CreateDescriptionMargin">  Stuff and things </p>
+        </div>           
       </div>
     );
   }
